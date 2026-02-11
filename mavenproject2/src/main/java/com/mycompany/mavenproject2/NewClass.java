cb8ff9ec065f23c712afe5d91a3fc0b0de828a95
ub-victor <victoireushindi371@gmail.com>
2026-02-11 14:44:25 +0200

    Lab 1

--- mavenproject2/src/main/java/com/mycompany/mavenproject2/NewClass.java
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/NewClass.java
@@ -13,7 +13,8 @@ public class NewClass {
         int i;
         for (i=1; i<=10; i++)
             {
-                if(i%2 == 0){
+                if(i%2 == 0)
+            {
             }{
                 continue;
             }
