45c3ef6c562545c507f7e99ae37f5d83c7af16d6
ub-victor <victoireushindi371@gmail.com>
2026-02-11 14:40:21 +0200

    Lab 1

--- mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -13,6 +13,7 @@ public class Mavenproject2 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         NewClass ite = new NewClass();
+        ite.terminate();
         
     }
 }
