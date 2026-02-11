ea9d213c9c4d43c6723e2b6b7389d4fc090e175e
ub-victor <victoireushindi371@gmail.com>
2026-02-11 15:44:23 +0200

    nest  loop

--- mavenproject2/src/main/java/com/mycompany/mavenproject2/Multiplicationtable.java
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/Multiplicationtable.java
@@ -8,7 +8,7 @@ public class Multiplicationtable {
         for (i = 1; i <= 12; i++) {
             for (j = 1; j <= 12; j++) {
                 int x = i * j;
-                System.out.println(i + " x " + j + " = " + i*j);
+                System.out.println(i + " x " + j + " = " + x);
             }
         }
     }
