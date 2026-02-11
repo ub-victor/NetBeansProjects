0010aea2d0a8e4c81f7dc9ac7c3405e75d168531
ub-victor <victoireushindi371@gmail.com>
2026-02-11 14:52:00 +0200

    Lab 1

--- mavenproject2/src/main/java/com/mycompany/mavenproject2/NewClass.java
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/NewClass.java
@@ -13,8 +13,7 @@ public class NewClass {
         int i;
         for (i=1; i<=10; i++){
             
-            if(i%2 == 0)
-            {            
+            if(i%2 == 0){                        
                 continue;
             }
             System.out.println("This is  iteration number"+ i);
