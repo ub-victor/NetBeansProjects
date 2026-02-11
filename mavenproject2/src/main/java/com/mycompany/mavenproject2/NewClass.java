a5e4e80431cf268d19f12609e5b11ca84baa76b5
ub-victor <victoireushindi371@gmail.com>
2026-02-11 14:38:51 +0200

    Lab 1

---
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/NewClass.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author victoire
+ */
+public class NewClass {
+    void terminate(){
+        int i;
+        for (i=1;i<=3;i++){
+            System.out.println("This is  iteration number"+ i);
+            if(i==5){
+                break;
+            }
+        }
+    }
+}
