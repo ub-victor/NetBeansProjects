b1b666ae5c0cf8d19aed5636ec97fe976ed08902
ub-victor <victoireushindi371@gmail.com>
2026-02-11 15:02:42 +0200

    Lab 1

--- mavenproject2/src/main/java/com/mycompany/mavenproject2/NewClass.java
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/NewClass.java
@@ -22,5 +22,11 @@ public class NewClass {
         }
     }
     void looping(){
+        int i,j;
+        for (i=1; i<=10; i++){
+            for (j=1; j<=10; j++){
+                
+            }
+        }
     }
 }
