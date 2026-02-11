d87876277f1903a56bbb92dda6cc2ace844862e4
ub-victor <victoireushindi371@gmail.com>
2026-02-11 16:10:32 +0200

    nest  loop

---
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/Method.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author victoire
+ */
+public class Method {
+    
+    public void helloWorld(){
+        System.out.println("Hello world");
+                
+    }
+    
+    public int sum(int a, int b, int sum){
+        a=10; b=10;
+        sum = a+b;
+        System.out.println(sum);
+        return sum;
+                
+    }
+    
+}
