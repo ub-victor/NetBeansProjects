1574f7fc13628b67a529a7f82d88ec90729aab89
ub-victor <victoireushindi371@gmail.com>
2026-02-18 16:13:58 +0200

    GUI in java

---
+++ GgroupUI/src/main/java/com/mycompany/ggroupui/GgroupUI.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ggroupui;
+
+/**
+ *
+ * @author victoire
+ */
+public class SimpleExample {
+    setTitle("Siple GUI Frame");
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
