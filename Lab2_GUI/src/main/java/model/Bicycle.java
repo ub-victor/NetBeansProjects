4d26532912ad3d8e44ddbc2977b6f5f211e92808
ub-victor <victoireushindi371@gmail.com>
2026-03-18 19:22:19 +0200

    Lab 2 add Mysql connector

--- Lab2_GUI/src/main/java/model/Bicycle.java
+++ Lab2_GUI/src/main/java/model/Bicycle.java
@@ -8,7 +8,7 @@ public class Bicycle {
     private int totalHours;
     private int amount;
 
-    // Constructor
+    // Constructor WITHOUT id (for save)
     public Bicycle(int startTime, int endTime, int totalHours, int amount) {
         this.startTime = startTime;
         this.endTime = endTime;
@@ -16,7 +16,17 @@ public class Bicycle {
         this.amount = amount;
     }
 
+    // Constructor WITH id (for update/delete)
+    public Bicycle(int id, int startTime, int endTime, int totalHours, int amount) {
+        this.id = id;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.totalHours = totalHours;
+        this.amount = amount;
+    }
+
     // Getters
+    public int getId() { return id; }
     public int getStartTime() { return startTime; }
     public int getEndTime() { return endTime; }
     public int getTotalHours() { return totalHours; }
