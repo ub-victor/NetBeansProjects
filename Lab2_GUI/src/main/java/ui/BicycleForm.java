f95fed36dbcaf4155bad9b494149c560acbe4bd6
ub-victor <victoireushindi371@gmail.com>
2026-03-19 17:48:04 +0200

    Design the bycycle

--- Lab2_GUI/src/main/java/ui/BicycleForm.java
+++ Lab2_GUI/src/main/java/ui/BicycleForm.java
@@ -97,9 +97,11 @@ public class BicycleForm extends javax.swing.JFrame {
         txtEnd.setPreferredSize(new java.awt.Dimension(100, 22));
 
         txtTotal.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 2, true));
+        txtTotal.setPreferredSize(new java.awt.Dimension(100, 22));
         txtTotal.addActionListener(this::txtTotalActionPerformed);
 
         txtAmount.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 2, true));
+        txtAmount.setPreferredSize(new java.awt.Dimension(100, 22));
         txtAmount.addActionListener(this::txtAmountActionPerformed);
 
         jLabel2.setFont(new java.awt.Font("Liberation Sans", 1, 24)); // NOI18N
