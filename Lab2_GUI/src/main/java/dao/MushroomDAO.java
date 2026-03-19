6869323256dfcb37e13f2523dc56c249d0d1afbe
ub-victor <victoireushindi371@gmail.com>
2026-03-19 18:52:08 +0200

    Mushroom implementation

--- Lab2_GUI/src/main/java/dao/MushroomDAO.java
+++ Lab2_GUI/src/main/java/dao/MushroomDAO.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dao;
 
-/**
- *
- * @author victoire
- */
+import model.Mushroom;
+import util.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class MushroomDAO {
-    
+
+    public void save(Mushroom m) {
+        try {
+            Connection conn = DBConnection.getConnection();
+
+            String sql = "INSERT INTO mushroom(name, has_gills, forest, has_ring, convex) VALUES(?,?,?,?,?)";
+
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ps.setString(1, m.getName());
+            ps.setBoolean(2, m.isHasGills());
+            ps.setBoolean(3, m.isForest());
+            ps.setBoolean(4, m.isHasRing());
+            ps.setBoolean(5, m.isConvex());
+
+            ps.executeUpdate();
+
+            System.out.println("Mushroom saved!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
