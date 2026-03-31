45ebcfa442556e9173708c2febb92a148c49679e
ub-victor <victoireushindi371@gmail.com>
2026-03-31 23:36:13 +0200

    Initial commit

--- FinanceManager/src/financemanager/models/User.java
+++ FinanceManager/src/financemanager/models/User.java
@@ -6,13 +6,47 @@ public class User {
     private String password;
     private String role;
 
-    // constructors, getters, setters
+    // Default constructor
     public User() {}
+
+    // Parameterized constructor
     public User(int id, String username, String password, String role) {
         this.id = id;
         this.username = username;
         this.password = password;
         this.role = role;
     }
-    // ... add getters and setters
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
