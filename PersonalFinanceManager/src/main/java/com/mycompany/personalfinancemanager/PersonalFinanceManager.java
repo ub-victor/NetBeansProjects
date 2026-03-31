483ef2267f352410ba401f2d1463a7b4858abb51
ub-victor <victoireushindi371@gmail.com>
2026-03-31 19:23:24 +0200

    Initial commit

---
+++ PersonalFinanceManager/src/main/java/com/mycompany/personalfinancemanager/PersonalFinanceManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.personalfinancemanager;
+
+/**
+ *
+ * @author victoire
+ */
+public class PersonalFinanceManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
