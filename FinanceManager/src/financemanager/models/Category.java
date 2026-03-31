0ac3f4593dce06a921ec200969783db7e6cbf7b3
ub-victor <victoireushindi371@gmail.com>
2026-03-31 23:36:27 +0200

    Initial commit

--- FinanceManager/src/financemanager/models/Category.java
+++ FinanceManager/src/financemanager/models/Category.java
@@ -5,5 +5,18 @@ public class Category {
     private String name;
     private String type; // "income" or "expense"
 
-    // constructors, getters, setters
+    public Category() {}
+
+    public Category(int id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
 }
