3c2c3473325461628c4996de21f962eabdda3c7a
ub-victor <victoireushindi371@gmail.com>
2026-03-31 23:08:26 +0200

    Initial commit

--- FinanceManager/src/financemanager/db/DatabaseConnection.java
+++ FinanceManager/src/financemanager/db/DatabaseConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/finance_manager";
-    private static final String USER = "root";       // change if needed
-    private static final String PASSWORD = "";       // set your MySQL password
+    private static final String USER = "root";       
+    private static final String PASSWORD = "Ushindi123!";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
