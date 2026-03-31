668a96772004eca3be9ca284893c9180803e694e
ub-victor <victoireushindi371@gmail.com>
2026-03-31 23:16:45 +0200

    Initial commit

--- FinanceManager/src/financemanager/models/Category.java
+++ FinanceManager/src/financemanager/models/Category.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package financemanager.models;
 
-/**
- *
- * @author victoire
- */
 public class Category {
-    
+    private int id;
+    private String name;
+    private String type; // "income" or "expense"
+
+    // constructors, getters, setters
 }
